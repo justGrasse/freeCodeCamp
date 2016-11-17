9888ca8a4051d9c643f0ea69d8c20b6e2e18ed75
Justin Grasse <just.grasse@gmail.com>
2016-11-16 20:40:54 -0800

    create a recursion method, has NOT been tested

--- advancedAlgorithms/permAlone.js
+++ advancedAlgorithms/permAlone.js
@@ -1,6 +1,23 @@
 
 function permAlone(str) {
+
+	// Array of ALL permutations
+	var permArr = [];
+
+	// aStr = the FINAL string; bStr = the unused letters
+	function recurStr(aStr, bStr){	
+		if (bStr.length == 1){
+			permArr.push(aStr + bStr);
+		}
+
+		for (var i in bStr){
+			bStr = bStr.replace(bStr[i],'');
+			recurStr((aStr+bStr[i]),bStr);
+		}
+	}
+
+
   return str;
 }
 
-permAlone('aab');
+permAlone('abc');
