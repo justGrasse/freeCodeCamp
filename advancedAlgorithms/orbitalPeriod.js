ada8b28be40ded8d5efa325d2dee82fe3c091cd3
Justin Grasse <just.grasse@gmail.com>
2016-11-18 12:01:52 -0800

    complete orbitalPeriod function

--- advancedAlgorithms/orbitalPeriod.js
+++ advancedAlgorithms/orbitalPeriod.js
@@ -2,7 +2,21 @@
 function orbitalPeriod(arr) {
   var GM = 398600.4418;
   var earthRadius = 6367.4447;
-  return arr;
+
+  var finalPeriod = [];
+
+
+  for (var i in arr){
+		var rad = earthRadius + arr[i]['avgAlt'];
+		var pi = Math.PI;
+		var orbPeriod = 2*pi*Math.sqrt(Math.pow(rad,3)/GM);
+		finalPeriod.push({});
+		finalPeriod[i]['name'] = arr[i]['name'];
+		finalPeriod[i]['orbitalPeriod'] = parseInt(Math.round(orbPeriod));
+  }
+
+  console.log(finalPeriod);
+  return finalPeriod;
 }
 
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
