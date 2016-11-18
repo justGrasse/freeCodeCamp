b4d89e3e89af78d51872a720e852ee4877b487f4
Justin Grasse <just.grasse@gmail.com>
2016-11-18 14:13:39 -0800

    complete paiwise function, last adv algorithm

--- advancedAlgorithms/pairwise.js
+++ advancedAlgorithms/pairwise.js
@@ -7,19 +7,42 @@ function pairwise(arr, arg) {
   if (arr.length <= 1){ return totalSum; }
 
   // Iterate through each element of array
-  for (var i in arr){
+  for (var i = 0; i < arr.length; i++){
+  	console.log("i is: " + i);
   	// If this value has already been used
-  	if (usedNumbers.indexOf(arr[i]) >=0){
+  	if (usedIndices.indexOf(i) >=0){
+  		console.log("Ignoring this one!");
   		// Ignore it
   		continue;
   	}
-  		// Ignore it
-  		if (arr[i] == arr[j]) { continue; }
+
+  	// Iterate through the following items in array
+  	for (var j = 1 + i; j < arr.length; j++){
+  		console.log("j is: " + j);
+  	// If this value has already been used
+  		if ((usedIndices.indexOf(j) >=0)||(usedIndices.indexOf(i)>=0)){
+  			console.log("Ignoring this one!");
+  			// Ignore it
+  			continue;
+  		}  		
+
+  		// If the sum is the result
+  		if (arr[i]+arr[j] == arg){
+  			console.log("arr[i] is: " + arr[i]);
+  			console.log("arr[j] is: " + arr[j]);
+  			// Add the indices to the total sum
+				totalSum += i + j;
+  			// Add the letters to the used array
+  			usedIndices.push(i,j);
+  		}
   	}
   }
 
+  console.log("totalSum is: " + totalSum);
+  console.log("usedIndices is: " + usedIndices); 
   return totalSum;
 }
 
 
-pairwise([1,4,2,3,0,5], 7);
+pairwise([1, 1, 1], 2);
+
