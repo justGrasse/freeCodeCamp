eee8f261ce8175dd4af1e17918bb8ac33dc927f0
Justin Grasse <just.grasse@gmail.com>
2016-11-30 20:40:00 -0800

    pseudocode - started

---
+++ calculator/calculator.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+	var num_str = "";	// Number String
+	var num_1, num_2;
+	var opr_str = "";	// Operator String
+
+/*
+	PSEUDO-CODE
+	- initialize num_str, op_str
+	- IF Number
+		- Push to num_str
+	- ELSE IF Operator AND opr_str.empty
+		- Note Operator
+		- Set num_1 = parseInt(num_str)
+		- Initialize num_str
+	- ELSE IF Operator 
+		- 
+ 
+	SCREEN:
+	- IF num_str is empty or 0
+		- Display 0
+	- ELSE 
+		- Display num_str
+
+					*/
+
+
+});
